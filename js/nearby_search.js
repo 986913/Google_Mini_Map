803acd16dc18005d9fd45138a0004dc821d81ea9
Ubuntu <ubuntu@ip-172-31-47-185.us-east-2.compute.internal>
2017-12-15 19:12:40 +0000

    Do place query on the front-end

--- js/nearby_search.js
+++ js/nearby_search.js
@@ -6,12 +6,26 @@ $(function(){
         var position = {lat: -25.363, lng: 131.044};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: DEFAULT_ZOOM,
-          center: position
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
+          center: position,
+	  positionn: position,
           map: map
         });
+
+   	$.ajax({
+		url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+		data:{
+			"location":position.lat + "," + position.lng,
+			"type":"restaurant",
+			"key":GOOGLE_API_KEY,
+			"radius":500
+		},
+		success: function(){
+		      debugger;
+		},
+		failure: function(){
+		     debugger;
+		}
+		})    
    }
 	initMap()
  })
