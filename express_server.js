e30a62e35b74acc248ba5bd38934675dea811b7c
Ubuntu <ubuntu@ip-172-31-47-185.us-east-2.compute.internal>
2017-12-15 18:09:41 +0000

    show the google map

--- express_server.js
+++ express_server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const path =require("path");
 
-app.get('/', function(req, res){
- res.sendFile(path.join(__dirname+'/templates/index.html'))
+app.get('/nearbysearch', function(req, res){
+ res.sendFile(path.join(__dirname+'/templates/nearby_search.html'));
 })
 app.use('/js', express.static(path.join(__dirname, 'js')))
 app.use('/css', express.static(path.join(__dirname, 'css')))
